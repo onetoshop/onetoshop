5853e511c6748a079fd97bad3b0dfaa8d7df9cdc
Gernandsr <gernand.sr.gar@gmail.com>
2019-10-29 12:35:05 +0100

    route contact en service aangemaakt

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -6,11 +6,30 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class Controller extends AbstractController
 {
+//    home pagina
     /**
      * @Route("/", name="homepage",)
      */
     public function homepage()
     {
-        return $this->render('base.html.twig');
+        return $this->render('homepage/homepage.html.twig');
+    }
+
+//    service pagina
+    /**
+     * @Route("/service", name="service",)
+     */
+    public function service()
+    {
+        return $this->render('service/service.html.twig');
+    }
+
+// contact pagina
+    /**
+     * @Route("/contact", name="contact",)
+     */
+    public function contact()
+    {
+        return $this->render('contact/contact.html.twig');
     }
 }
