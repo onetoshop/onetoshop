d5a2512cf28ac4313bf24a2405501939d2eb33b2
Your Name <jesseonetoweb@gmail.com>
2019-11-12 11:33:08 +0100

    logo gefixed

--- config/bundles.php
+++ config/bundles.php
@@ -8,7 +8,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
@@ -16,4 +15,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
---
+++ src/Entity/Aanmelding.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AanmeldingRepository")
+ */
+class Aanmelding
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $voornaam;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $achternaam;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $telefoon;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $bereik;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $keuz;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVoornaam(): ?string
+    {
+        return $this->voornaam;
+    }
+
+    public function setVoornaam(string $voornaam): self
+    {
+        $this->voornaam = $voornaam;
+
+        return $this;
+    }
+
+    public function getAchternaam(): ?string
+    {
+        return $this->achternaam;
+    }
+
+    public function setAchternaam(string $achternaam): self
+    {
+        $this->achternaam = $achternaam;
+
+        return $this;
+    }
+
+    public function getTelefoon(): ?string
+    {
+        return $this->telefoon;
+    }
+
+    public function setTelefoon(string $telefoon): self
+    {
+        $this->telefoon = $telefoon;
+
+        return $this;
+    }
+
+    public function getBereik(): ?string
+    {
+        return $this->bereik;
+    }
+
+    public function setBereik(string $bereik): self
+    {
+        $this->bereik = $bereik;
+
+        return $this;
+    }
+
+    public function getKeuz(): ?string
+    {
+        return $this->keuz;
+    }
+
+    public function setKeuz(string $keuz): self
+    {
+        $this->keuz = $keuz;
+
+        return $this;
+    }
+}
