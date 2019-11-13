aa053ff7887c3e62e30dec5d19564f3cc73683d5
Your Name <jesseonetoweb@gmail.com>
2019-11-13 15:47:49 +0100

    admin versie 4

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -3,31 +3,58 @@
 namespace App\Controller;
 
 use App\Entity\Aanmeld;
+use App\Entity\Card;
+use App\Entity\Gegeven;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\AanmeldType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class BackendController extends AbstractController
 {
 
-//    /**
-//     * @Route("/admin", name="admin")
-//     */
-//    public function index()
-//    {
-//        return $this->render('admin/index.html.twig');
-//    }
     /**
      * @Route("/admin", name="admin")
      */
-    public function show()
+    public function index()
     {
+        # get amount of signups
+        $em = $this->getDoctrine()->getManager();
+        $repoArticles = $em->getRepository(Aanmeld::class);
+        $totaleaanmeld = $repoArticles->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+
+        # get all signups
         $aanmeld = $this->getDoctrine()->getRepository(Aanmeld::class)->findAll();
+
+        # get amount of articles
+        $em = $this->getDoctrine()->getManager();
+        $repoArticles = $em->getRepository(Gegeven::class);
+        $gegeven = $repoArticles->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+
+        # get amount of users
+        $em = $this->getDoctrine()->getManager();
+        $repoArticles = $em->getRepository(User::class);
+        $users = $repoArticles->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+
+        # get amount of cads
+        $em = $this->getDoctrine()->getManager();
+        $repoArticles = $em->getRepository(Card::class);
+        $cards = $repoArticles->createQueryBuilder('a')->select('count(a.id)')->getQuery()->getSingleScalarResult();
+
+
         return $this->render('admin/index.html.twig', [
-            'aanmeldingen' => $aanmeld
+            'totaleaanmeld' => $totaleaanmeld,
+            'aanmeldingen' => $aanmeld,
+            'gegeven' => $gegeven,
+            'users' => $users,
+            'cards' => $cards
         ]);
     }
+
+
+//
 }
 
 
