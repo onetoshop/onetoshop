a09c2386f64d79dc386032440cb974300202164b
Your Name <jesseonetoweb@gmail.com>
2019-11-26 10:18:37 +0100

    blog rout fix

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -15,7 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/{_locale}/blogs", name="blogs")
+     * @Route("/{_locale}/blog", name="blogs")
      */
     public function index()
     {
