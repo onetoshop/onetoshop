ce94cde506e0ca553359c7c785e8f7e75a3a07fd
Your Name <jesseonetoweb@gmail.com>
2019-11-29 16:36:48 +0100

    home

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -8,6 +8,7 @@ use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\PieChart;
 
 class MailController extends AbstractController
 {
@@ -15,29 +16,27 @@ class MailController extends AbstractController
 //    /**
 //     * @Route("/{_locale}/mailen", name="mailen")
 //     */
-//    public function index(Request $request, \Swift_Mailer $mailer)
+//    public function indexAction()
 //    {
+//        $pieChart = new PieChart();
+//        $pieChart->getData()->setArrayToDataTable(
+//            [['Task', 'Hours per Day'],
+//                ['Work',     11],
+//                ['Eat',      2],
+//                ['Commute',  2],
+//                ['Watch TV', 2],
+//                ['Sleep',    7]
+//            ]
+//        );
+//        $pieChart->getOptions()->setTitle('My Daily Activities');
+//        $pieChart->getOptions()->setHeight(500);
+//        $pieChart->getOptions()->setWidth(900);
+//        $pieChart->getOptions()->getTitleTextStyle()->setBold(true);
+//        $pieChart->getOptions()->getTitleTextStyle()->setColor('#009900');
+//        $pieChart->getOptions()->getTitleTextStyle()->setItalic(true);
+//        $pieChart->getOptions()->getTitleTextStyle()->setFontName('Arial');
+//        $pieChart->getOptions()->getTitleTextStyle()->setFontSize(20);
 //
-//
-//        $form = $this->createForm(ReplyType::class);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//
-//            $form = $form->getData();
-//
-//            $message = (new \Swift_Message('Bericht van Onetoshop'))
-//                ->setFrom('dummyonetoshop@gmail.com')
-//                ->setSubject($form['Onderwerp'])
-//                ->setTo($form['Geadresseerde'])
-//                ->setBody($form['Bericht'], 'text/html');
-//            $mailer->send($message);
-//
-//
-//
-//            return $this->redirectToRoute('mailen');
-//    }
-//        return $this->render('mailtesten.html.twig',[
-//        'form' => $form->createView()]);
+//        return $this->render('mailtesten.html.twig', array('piechart' => $pieChart));
 //    }
 }
