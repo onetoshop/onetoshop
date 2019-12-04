32be93d85fb73c0e6ebb4d2b141aa39a6e9cd149
Gernandsr <gernand.sr.gar@gmail.com>
2019-12-04 11:19:32 +0100

    update project page

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -1,34 +1,34 @@
 <?php
-//
-//namespace App\Controller;
-//
-//use App\Entity\Blog;
-//use App\Entity\Reply;
-//use App\Form\ReplyType;
-//use FOS\CKEditorBundle\Form\Type\CKEditorType;
-//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-//use Symfony\Component\Routing\Annotation\Route;
-//use Symfony\Component\HttpFoundation\Request;
-//use CMEN\GoogleChartsBundle\GoogleCharts\Charts\PieChart;
-//
-//class MailController extends AbstractController
-//{
-//
-//    /**
-//     * @Route("/{_locale}/mailen", name="mailen")
-//     */
-//    public function indexAction()
-//    {
-//
-//        $blog = $this->getDoctrine()->getRepository(Blog::class)->findBy(
-//            array(),
-//            array('id' => 'DESC'),
-//            3,
-//            0);
-//
-//
-//        return $this->render('mailtesten.html.twig', [
-//            'blogs' => $blog
-//        ]);
-//    }
+
+namespace App\Controller;
+
+use App\Entity\Blog;
+use App\Entity\Reply;
+use App\Form\ReplyType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\PieChart;
+
+class MailController extends AbstractController
+{
+
+    /**
+     * @Route("/{_locale}/mailen", name="mailen")
+     */
+    public function indexAction()
+    {
+
+        $blog = $this->getDoctrine()->getRepository(Blog::class)->findBy(
+            array(),
+            array('id' => 'DESC'),
+            3,
+            0);
+
+
+        return $this->render('mailtesten.html.twig', [
+            'blogs' => $blog
+        ]);
+    }
 }
