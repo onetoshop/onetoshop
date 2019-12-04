5c374ab1bb8de8e2b347a88252ddc8e3ea8464e3
Your Name <jesseonetoweb@gmail.com>
2019-12-04 10:55:41 +0100

    blog equal height

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -1,42 +1,34 @@
 <?php
-
-namespace App\Controller;
-
-use App\Entity\Reply;
-use App\Form\ReplyType;
-use FOS\CKEditorBundle\Form\Type\CKEditorType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use CMEN\GoogleChartsBundle\GoogleCharts\Charts\PieChart;
-
-class MailController extends AbstractController
-{
+//
+//namespace App\Controller;
+//
+//use App\Entity\Blog;
+//use App\Entity\Reply;
+//use App\Form\ReplyType;
+//use FOS\CKEditorBundle\Form\Type\CKEditorType;
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\Routing\Annotation\Route;
+//use Symfony\Component\HttpFoundation\Request;
+//use CMEN\GoogleChartsBundle\GoogleCharts\Charts\PieChart;
+//
+//class MailController extends AbstractController
+//{
 //
 //    /**
 //     * @Route("/{_locale}/mailen", name="mailen")
 //     */
 //    public function indexAction()
 //    {
-//        $pieChart = new PieChart();
-//        $pieChart->getData()->setArrayToDataTable(
-//            [['Task', 'Hours per Day'],
-//                ['Work',     11],
-//                ['Eat',      2],
-//                ['Commute',  2],
-//                ['Watch TV', 2],
-//                ['Sleep',    7]
-//            ]
-//        );
-//        $pieChart->getOptions()->setTitle('My Daily Activities');
-//        $pieChart->getOptions()->setHeight(500);
-//        $pieChart->getOptions()->setWidth(900);
-//        $pieChart->getOptions()->getTitleTextStyle()->setBold(true);
-//        $pieChart->getOptions()->getTitleTextStyle()->setColor('#009900');
-//        $pieChart->getOptions()->getTitleTextStyle()->setItalic(true);
-//        $pieChart->getOptions()->getTitleTextStyle()->setFontName('Arial');
-//        $pieChart->getOptions()->getTitleTextStyle()->setFontSize(20);
 //
-//        return $this->render('mailtesten.html.twig', array('piechart' => $pieChart));
+//        $blog = $this->getDoctrine()->getRepository(Blog::class)->findBy(
+//            array(),
+//            array('id' => 'DESC'),
+//            3,
+//            0);
+//
+//
+//        return $this->render('mailtesten.html.twig', [
+//            'blogs' => $blog
+//        ]);
 //    }
 }
