1ea3d420d8fba07f4f3e402c1ac6867fa95edbed
Gernandsr <gernand.sr.gar@gmail.com>
2020-01-10 15:42:10 +0100

    update appinfo en apps uit 1 datarow

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -34,7 +34,7 @@ class AppController extends AbstractController
      * @Route("/{_locale}/apps/{slug}", name="appinfo")
      */
     public function appinfo($slug){
-        $appinfo = $this->getDoctrine()->getRepository(Appinfo::class)->findBy([
+        $appinfo = $this->getDoctrine()->getRepository(Apps::class)->findBy([
             'groep' => $slug
         ]);
 
