6e83e99903a9b56834264f5648e9c4f99ecab4a5
Gernandsr <gernand.sr.gar@gmail.com>
2020-01-23 10:43:19 +0100

    apps volledig af

--- src/Form/AppType.php
+++ src/Form/AppType.php
@@ -12,30 +12,14 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AppsType extends AbstractType
+class AppType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('image', ImageType::class, [
-                'label' => false,
-                'required'   => false,
-            ])
             ->add('beschrijving', CKEditorType::class, [
                 'label'=> false,
                 'required'   => false,
-            ])
-
-            ->add('parent', EntityType::class, [
-                'required' => false,
-                'class' => Apps::class,
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('u')
-                      ->orderBy('u.parent', 'ASC');
-                },
-                'choice_label' => 'naam',
-                'choice_value' => 'id',
-                'label' => false,
             ])
             ->add('naam', TextType::class, [
                 'label'=> false,
